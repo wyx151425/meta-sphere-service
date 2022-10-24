10d346aab11996f466a80b83fa3f1ef80fc57c44
wyx151425 <wyx151425@foxmail.com>
2022-10-24 19:48:47 +0800

    2022/10/24 初始化版本控制

---
+++ src/main/java/com/rumofuture/metasphereservice/MetaSphereServiceApplication.java
@@ -0,0 +1,13 @@
+package com.rumofuture.metasphereservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetaSphereServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MetaSphereServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rumofuture/metasphereservice/MetaSphereServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rumofuture.metasphereservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetaSphereServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
