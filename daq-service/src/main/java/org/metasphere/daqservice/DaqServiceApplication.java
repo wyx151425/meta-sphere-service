b0a370de2141ad0865cbd0dc8f32018126e108bd
wyx151425 <wyx151425@foxmail.com>
2023-02-22 20:58:32 +0800

    2023/02/22 添加数据采集服务

---
+++ daq-service/src/main/java/org/metasphere/daqservice/DaqServiceApplication.java
@@ -0,0 +1,13 @@
+package org.metasphere.daqservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaqServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DaqServiceApplication.class, args);
+    }
+
+}
---
+++ daq-service/src/test/java/org/metasphere/daqservice/DaqServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.metasphere.daqservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaqServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
