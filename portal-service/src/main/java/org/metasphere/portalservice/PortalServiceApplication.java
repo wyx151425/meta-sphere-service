7d922ebfb84df23d4b3a7c3673df765e17e405a3
wyx151425 <wyx151425@foxmail.com>
2023-12-15 21:23:28 +0800

    2023/12/15 添加Portal服务启动类

---
+++ portal-service/src/main/java/org/metasphere/portalservice/PortalServiceApplication.java
@@ -0,0 +1,18 @@
+package org.metasphere.portalservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: WangZhenqi
+ * @Description:
+ * @Date: Created in 2023-12-15 16:38
+ * @Modified By:
+ */
+@SpringBootApplication
+public class PortalServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PortalServiceApplication.class, args);
+    }
+}
